0e9c040bd8f6f07ecaacbe18fd5314ac396882ef
Eertmans <jeertmans@icloud.com>
2020-10-07 19:29:03 +0200

    decorators tests

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -23,6 +23,14 @@ def f_mul_int_missing_all(a, b):
     return f_mul(a, b)
 
 
+def f_mul_int_typed_kwd(a: int, b: int = 0) -> float:
+    return f_mul(a, b)
+
+
+class SubInt(int):
+    pass
+
+
 class TestDecorators(TestCase):
     def test_assert_correct_typing(self):
 
@@ -30,6 +38,7 @@ class TestDecorators(TestCase):
         f_1 = assert_correct_typing(f_mul_int_missing_one)
         f_2 = assert_correct_typing(f_mul_int_missing_two)
         f_a = assert_correct_typing(f_mul_int_missing_all)
+        f_k = assert_correct_typing(f_mul_int_typed_kwd)
 
         # 1. Check with correct typing
 
@@ -39,6 +48,8 @@ class TestDecorators(TestCase):
         f_1(a, b)
         f_2(a, b)
         f_a(a, b)
+        f_k(a, b)
+        f_k(a, b=b)
 
         # 2. Check with incorrect typing
         assert_msg = "AssertionError should have been raised"
@@ -46,60 +57,107 @@ class TestDecorators(TestCase):
 
         a, b = 1, 0.5
 
+        error = None
         try:
             f(a, b)
-            error = None
         except AssertionError as e:
             error = e
         assert isinstance(error, AssertionError), assert_msg
 
+        error = None
         try:
             f_1(a, b)
-            error = None
         except AssertionError as e:
             error = e
         self.assertEqual(error, None, msg=no_assert_msg)
 
+        error = None
         try:
             f_2(a, b)
-            error = None
         except AssertionError as e:
             error = e
         self.assertEqual(error, None, msg=no_assert_msg)
 
+        error = None
         try:
             f_a(a, b)
-            error = None
         except AssertionError as e:
             error = e
         self.assertEqual(error, None, msg=no_assert_msg)
+
+        error = None
+        try:
+            f_k(a, b)
+        except AssertionError as e:
+            error = e
+        assert isinstance(error, AssertionError), assert_msg
+
+        error = None
+        try:
+            f_k(a, b=b)
+        except AssertionError as e:
+            error = e
+        assert isinstance(error, AssertionError), assert_msg
 
         a, b = 1, 1j
 
+        error = None
         try:
             f(a, b)
-            error = None
         except AssertionError as e:
             error = e
         assert isinstance(error, AssertionError), assert_msg
 
+        error = None
         try:
             f_1(a, b)
-            error = None
         except AssertionError as e:
             error = e
         assert isinstance(error, AssertionError), assert_msg
 
+        error = None
         try:
             f_2(a, b)
-            error = None
         except AssertionError as e:
             error = e
         assert isinstance(error, AssertionError), assert_msg
 
+        error = None
         try:
             f_a(a, b)
-            error = None
         except AssertionError as e:
             error = e
         self.assertEqual(error, None, msg=no_assert_msg)
+
+        error = None
+        try:
+            f_k(a, b)
+        except AssertionError as e:
+            error = e
+        assert isinstance(error, AssertionError), assert_msg
+
+        error = None
+        try:
+            f_k(a, b=b)
+        except AssertionError as e:
+            error = e
+        assert isinstance(error, AssertionError), assert_msg
+
+        # 3. Check with subclasses
+
+        a, b = SubInt(1), SubInt(2)
+
+        f(a, b)
+
+        # 4. Check unpacked multiple errors
+
+        f_u = assert_correct_typing(f, join=False)
+
+        a, b = 1.0, 2.0
+
+        error = None
+        try:
+            f_u(a, b)
+        except AssertionError as e:
+            error = e
+        assert isinstance(error, AssertionError), assert_msg
