81d55c65c0627f23f1830d0e9264ca6b54ae79bb
Eertmans <jeertmans@icloud.com>
2020-10-07 19:29:50 +0200

    docu. setup

--- pysign/output.py
+++ pysign/output.py
@@ -2,23 +2,50 @@ import warnings
 import sys
 
 
-def raise_warning(msg):
+def raise_warning(msg: str) -> None:
+    """
+    Raises a warning with given message.
+
+    :param msg: the message
+    """
     warnings.warn(msg, Warning)
 
 
-def raise_assertion_error(msg):
+def raise_assertion_error(msg: str) -> None:
+    """
+    Raises an assertion error with given message.
+
+    :param msg: the message
+    :raises: AssertionError
+    """
     raise AssertionError(msg)
 
 
-def raise_type_error(msg):
+def raise_type_error(msg: str) -> None:
+    """
+    Raises a type error with given message.
+
+    :param msg: the message
+    :raises: TypeError
+    """
     raise TypeError(msg)
 
 
-def raise_stdout(msg):
+def raise_stdout(msg: str) -> None:
+    """
+    Prints a message in the standard output.
+
+    :param msg: the message
+    """
     print(msg)
 
 
-def raise_stderr(msg):
+def raise_stderr(msg: str) -> None:
+    """
+    Prints a message in the error output.
+
+    :param msg: the message
+    """
     print(msg, file=sys.stderr)
 
 
